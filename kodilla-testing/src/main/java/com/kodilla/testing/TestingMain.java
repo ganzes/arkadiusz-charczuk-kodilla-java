ac4bb3efe896e51ebf7fcf60ef9685259e5548e3
ganzes <charczuk.arkadiusz@gmail.com>
2019-03-27 04:18:14 +0100

    Test TDD Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator testCalculator = new Calculator();//utworzenie obiektu testCalculator, ktory posiada cechy/metody klasy Calculator
+
+        int add = testCalculator.addition(6,4);//dodawanie, wywolanie metody dodawania obiektu tC klasy C
+        int sub = testCalculator.subtract(10,6);//odejmowanie, wywolanie metody odejmowania obiektu tC klasy C
+
+        if (testCalculator.addition(6,4) == 10){
+            System.out.println("Test na dodawanie ok");
+        } else {
+            System.out.println("Test na dodawanie nie ok");
+        }
+
+        if (testCalculator.subtract(10,6) == 4){
+            System.out.println("Test na odejmowanie ok");
+        } else {
+            System.out.println("Test na odejmowanie nie ok");
+        }
+
+        System.out.println("Wynik sprawdzający dodawanie: " + add);
+        System.out.println("Wynik sprawdzajacy odejmowanie: " + sub);
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addition(int a, int b){//metoda dodawania
+        return a + b;
+    }
+
+    public int subtract (int a, int b){//metoda odejmowania
+        return a - b;
+    }
+
+}
