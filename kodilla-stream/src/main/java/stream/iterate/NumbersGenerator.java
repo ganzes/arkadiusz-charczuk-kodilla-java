55ed3783c521697f56cf525fbfa69977836105b7
ganzes <charczuk.arkadiusz@gmail.com>
2019-04-17 22:06:19 +0200

    Zadanie: Niemutowalny użytkownik forum

---
+++ kodilla-stream/src/main/java/stream/iterate/NumbersGenerator.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream.iterate;
+
+import java.util.stream.*;//importujemy bibliotekę java.util.stream
+
+public final class NumbersGenerator {
+    public static void generateEven(int max) {//tworzymy statyczną metodę generateEven(int max), której zadaniem
+        // jest wygenerowanie kolejnych liczb parzystych.
+        //Metoda otrzymuje jeden argument wejściowy max mówiący o tym ile liczb 'pozwalamy'
+        // wygenerować naszemu strumieniowi Stream.
+        Stream.iterate(1, n -> n + 1)//wywołujemy metodę statyczną iterate(Integer seed, UnaryOperator f) interfejsu Stream.
+                .limit(max)
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
