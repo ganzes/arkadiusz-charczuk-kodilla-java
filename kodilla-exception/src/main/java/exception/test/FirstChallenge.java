84c6ba46b9c3f6f374b88c4c8c1c55e40a9637c8
ganzes <charczuk.arkadiusz@gmail.com>
2019-05-03 19:17:31 +0200

    update

--- kodilla-exception/src/main/java/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/exception/test/FirstChallenge.java
@@ -3,17 +3,8 @@ package exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-
-            if(b == 0){
-                throw new ArithmeticException();
-            }
-
-        } catch ( ArithmeticException e) {
-            System.out.println ( "Error: can't divide by 0, error " + e );
-        } finally {
-            System.out.println ( "Result of a divide between " + a + " and " + b + " is: " );
+        if(b == 0){
+            throw new ArithmeticException();
         }
         return a / b;
     }
@@ -26,9 +17,16 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
 
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
 
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println ( "Upsi! Error! " + e );
+        }
+        finally {
+            System.out.println ( "Your result is above!" );
+        }
     }
 }
