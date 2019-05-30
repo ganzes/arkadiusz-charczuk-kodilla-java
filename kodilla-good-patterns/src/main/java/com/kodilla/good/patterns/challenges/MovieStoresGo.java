bc91f6fe06b0f49539ac3f70b3e79666b07e644b
ganzes <charczuk.arkadiusz@gmail.com>
2019-05-30 21:00:06 +0200

    chalennge1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoresGo.java
@@ -0,0 +1,18 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoresGo {
+    public static void main (String [] args){
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.getMovies());
+
+        String challengeMovieStore = movieStore.getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(challengeMovieStore);
+
+    }
+}
