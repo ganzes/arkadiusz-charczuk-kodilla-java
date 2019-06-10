226c4ef751fa946dc2e1017f936848ba3143a0b6
ganzes <charczuk.arkadiusz@gmail.com>
2019-06-10 20:23:20 +0200

    Zadanie: Tablica z listami zadań added tasks

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -8,14 +8,22 @@ public final class Board {
     }
 
     public void toDoList(){
+        taskList.getTasks().add("Task 1");
         System.out.println("To do list: " + taskList.getTasks());
+
     }
 
     public void inProgressList(){
-        System.out.println("In progress list: " + taskList.getTasks());
+        taskList.getTasks().add("Task 2");
+
+         System.out.println("In progress list: " + taskList.getTasks());
+
     }
 
     public void doneList(){
+        taskList.getTasks().add("Task 3");
+
         System.out.println("Done list: " + taskList.getTasks());
+
     }
 }
