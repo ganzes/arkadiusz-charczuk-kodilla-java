de177c4753a631ef2cfc455aa3789cf7927db9ec
ganzes <charczuk.arkadiusz@gmail.com>
2019-09-23 13:59:13 +0200

    24.1 update - added assertion

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
@@ -4,11 +4,14 @@ import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
 import com.kodilla.hibernate.manytomany.dao.CompanyDao;
 import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,35 +24,29 @@ public class FacadeTestSuite {
     private EmployeeDao employeeDao;
 
     @Test
-    public void testGetCompanyBySomeName() {
+    public void testGetCompanyBySomeName() throws NotFoundExceptionCE {
         //Given
         Company dataMaesters = new Company("Data Maesters");
         companyDao.save(dataMaesters);
         //When
         int dataMaestersId = dataMaesters.getId();
+        List<Company> companiesBySomeName = facade.getCompanyBySomeName("aest");
         //Then
-        try {
-            facade.getCompanyBySomeName("aest");
-        } catch (NotFoundExceptionCE e) {
-            //bussiness exception - should be handled in real application
-        }
+        Assert.assertEquals(1, companiesBySomeName.size());
         //CleanUp
         companyDao.deleteById(dataMaestersId);
     }
 
     @Test
-    public void testGetEmployeeBySomeName(){
+    public void testGetEmployeeBySomeName() throws NotFoundExceptionCE {
         //Given
         Employee johnSmith = new Employee("John", "Smith");
         employeeDao.save(johnSmith);
         //When
         int nameID = johnSmith.getId();
+        List<Employee> employeesBySomeName = facade.getEmployeeBySomeName("ith");
         //Then
-        try {
-            facade.getEmployeeBySomeName("ith");
-        } catch (NotFoundExceptionCE e) {
-            //bussiness exception - should be handled in real application
-        }
+        Assert.assertEquals(1, employeesBySomeName.size());
         //CleanUp
         employeeDao.deleteById(nameID);
     }
