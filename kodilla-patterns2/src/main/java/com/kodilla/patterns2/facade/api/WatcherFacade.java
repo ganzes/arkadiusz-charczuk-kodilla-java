5893eafe0e3e23b29e0c518b60751fe659c935f9
ganzes <charczuk.arkadiusz@gmail.com>
2019-09-25 04:01:27 +0200

    24.3 - Zadanie: Dodajemy logowanie żądań do fasady

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/WatcherFacade.java
@@ -0,0 +1,59 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Aspect
+public class WatcherFacade {
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.kodilla.patterns2.facade.api.WatcherFacade.class);
+    int count = 0;
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTimeSuccessfulOrder(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time to complete your order was: " + (end - begin) + "[ms]" + " so nice one!");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logBefore(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Order: " + order + ", UserId: " + userId);
+        LOGGER.info("Method started!");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logAfterReturing(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Order: " + order + ", UserId: " + userId);
+        LOGGER.info("Method ended without any errors!");
+    }
+
+    @AfterThrowing("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logAfterThrowing(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Order: " + order + ", UserId: " + userId);
+        LOGGER.info("Method threw an exception!");
+    }
+
+    @After("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logAfter(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Order: " + order + ", UserId: " + userId);
+        LOGGER.info("Method ended!");
+    }
+}
